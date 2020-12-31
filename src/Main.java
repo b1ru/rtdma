b534bcf9647a599bd253ddbadbcd0a27c16c077b
Bill Tsek <billtsekev@gmail.com>
2020-12-31 16:56:36 +0200

    Many changes

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,28 @@ public class Main {
     private static final int numberOfNodes = 8;
     private static final int numberOfChannels = 4;
     private static final int numberOfSlots = 100000;
+    private static long slotDuration = 0;
 
-    private static Random rand = new Random();              // random number generator
+    public static void main(String[] args) throws InterruptedException {
+        simulation();   // run simulation one time to find the slot duration
+        simulation();   // run simulation a second time, for the actual simulation
+    }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static int getNumberOfNodes(){
+        return numberOfNodes;
+    }
+
+    public static int getNumberOfChannels() { return numberOfChannels; }
+
+    public static int getNumberOfSlots() { return numberOfSlots; }
+
+    public static void setSlotDuration(long slotDuration) {
+        Main.slotDuration = slotDuration;
+    }
+
+    public static long getSlotDuration(){ return slotDuration; }
+
+    private static void simulation() throws InterruptedException {
         // choose configuration
         //   1: transmitter can be tuned to 2 channels, 2 receivers
         //   2: transmitter can be tuned to 1 channel,  4 receivers
@@ -18,59 +36,45 @@ public class Main {
         // create nodes
         Node[] nodes = new Node[numberOfNodes];
         for (int i=0; i<nodes.length; i++){
-            nodes[i] = new Node(id(i),configuration);
+            nodes[i] = new Node(i, configuration, 5, slotDuration);
         }
-        // A and B are Lists of Sets
-        // A[i] is the set of nodes that can transmit to channel i
-        // B[i] is the set of nodes that can receive from channel i
-        List<Set<Integer>> A = new ArrayList<>();
-        List<Set<Integer>> B = new ArrayList<>();
+        // A and B are Lists of Lists
+        // A[i] is the List of nodes that can transmit to channel i
+        // B[i] is the List of nodes that can receive from channel i
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
 
         // Initialize A and B
         for (int i=0; i<numberOfChannels; i++){
-            A.add(new HashSet<>());
-            B.add(new HashSet<>());
+            A.add(new ArrayList<>());
+            B.add(new ArrayList<>());
         }
 
         // find A and B
         for (int i=0; i<A.size(); i++){
             for (int j=0; j<nodes.length; j++){
-                if (nodes[j].getT().contains(id(i))){
-                    A.get(i).add(id(j));
+                if (nodes[j].getT().contains(i)){
+                    A.get(i).add(j);
                 }
-                if (nodes[j].getR().contains(id(i))){
-                    B.get(i).add(id(j));
+                if (nodes[j].getR().contains(i)){
+                    B.get(i).add(j);
                 }
             }
         }
 
-//        // print A and B
-//        System.out.println("A: " + A);
-//        System.out.println("B: " + B);
+        // pass A and B to all Nodes
+        for (int i=0; i<nodes.length; i++){
+            nodes[i].setA(A);
+            nodes[i].setB(B);
+        }
 
-
-        // start the simulation
-        for (int slot=1; slot<=numberOfSlots; slot++){
-            // start all threads
-            for (int i=0; i<nodes.length; i++){
-                nodes[i].start();
-            }
-            // wait all threads to finish
-            for (int i=0; i<nodes.length; i++){
-                nodes[i].join();
-            }
+        // start all threads
+        for (int i=0; i<nodes.length; i++){
+            nodes[i].start();
+        }
+        // wait all threads to finish
+        for (int i=0; i<nodes.length; i++){
+            nodes[i].join();
         }
     }
-
-    public static int getNumberOfSlots(){
-        return numberOfSlots;
-    }
-    public static int getNumberOfChannels() { return numberOfChannels; }
-    private static int id(int pos){
-        // Node IDs are numbered starting from 1
-        // Arrays indexing starts from 0
-        // Given an array index, return the relevant ID
-        return pos + 1;
-    }
-
 }
--- src/Node.java
+++ src/Node.java
@@ -1,30 +1,53 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 public class Node extends Thread {
     private LinkedList<Packet> queue;               // the node's buffer
     private Set<Integer> T;                         // channels the node can transmit to
     private Set<Integer> R;                         // channels the node can receive from
-    //private int id;                                 // the unique id of the node
+    private ArrayList<ArrayList<Integer>> A;
+    private ArrayList<ArrayList<Integer>> B;
+    private Random rand;                            // random number generator
+    private long slotDuration;                      // timeslot duration
 
-    public Node(int id, int configuration){
+    public Node(int id, int configuration, long seed, long slotDuration){
         queue = new LinkedList<>();
         T = new HashSet<>();
         R = new HashSet<>();
+        rand = new Random(seed);
+        this.slotDuration = slotDuration;
         configure(id, configuration);
     }
 
     public void run(){
-        // run the slot transmission algorithm
-
-        // initialize the set of all the channels
-        Set<Integer> channels = new HashSet<>();
-        for (int channel=1; channel<=Main.getNumberOfChannels(); channel++){
-            channels.add(channel);
+        if (slotDuration==0){           // find slot duration and set it to main
+            long maxTime=0;
+            double factor=2;          // timeslot duration = ceil(factor * maximum slot time)
+            for (int slot=0; slot<Main.getNumberOfSlots(); slot++) {
+                long startTime = System.currentTimeMillis();
+                slotAction();
+                long time = System.currentTimeMillis() - startTime;
+                if (time > maxTime) {
+                    maxTime = time;
+                }
+            }
+            synchronized(this){
+                if (maxTime*factor > Main.getSlotDuration()){
+                    Main.setSlotDuration((long) Math.ceil(maxTime*factor));
+                }
+            }
+        } else{                     // do the actual simulation
+            for (int slot=0; slot<Main.getNumberOfSlots(); slot++){
+                slotAction();
+            }
         }
-        // make the transmission
+    }
 
+    public void setA(ArrayList<ArrayList<Integer>> A) {
+        this.A = A;
+    }
+
+    public void setB(ArrayList<ArrayList<Integer>> B) {
+        this.B = B;
     }
 
     public Set<Integer> getT() {
@@ -44,27 +67,27 @@ public class Node extends Thread {
                 //   each node has two receivers
 
                 // configure the transmission range
-                if (id==1 || id==2 || id==3){
+                if (id==0 || id==1 || id==2){
+                    T.add(0);
+                    T.add(1);
+                } else if (id==3 || id==4 || id==5){
                     T.add(1);
                     T.add(2);
-                } else if (id==4 || id==5 || id==6){
+                } else if (id==6 || id==7){
                     T.add(2);
                     T.add(3);
-                } else if (id==7 || id==8){
-                    T.add(3);
-                    T.add(4);
                 }
 
                 // configure the receivers
-                if (id==1){
+                if (id==0){
+                    R.add(1);
                     R.add(2);
-                    R.add(3);
-                } else if (id==2 || id==3 || id==5 || id==7){
-                    R.add(2);
-                    R.add(4);
-                } else if (id==4 || id==6 || id==8){
+                } else if (id==1 || id==2 || id==4 || id==6){
                     R.add(1);
                     R.add(3);
+                } else if (id==3 || id==5 || id==7){
+                    R.add(0);
+                    R.add(2);
                 }
                 break;
             case 2:
@@ -73,21 +96,21 @@ public class Node extends Thread {
                 //   each node has four receivers, one for each wavelength
 
                 // configure the transmission range
-                if (id==1 || id==2){
+                if (id==0 || id==1){
+                    T.add(0);
+                } else if (id==2 || id==3){
                     T.add(1);
-                } else if (id==3 || id==4){
+                } else if (id==4 || id ==5){
                     T.add(2);
-                } else if (id==5 || id ==6){
+                } else if (id==6 || id==7){
                     T.add(3);
-                } else if (id==7 || id==8){
-                    T.add(4);
                 }
 
                 // configure the receivers
+                R.add(0);
                 R.add(1);
                 R.add(2);
                 R.add(3);
-                R.add(4);
                 break;
             case 3:
                 // system configuration 3
@@ -95,22 +118,70 @@ public class Node extends Thread {
                 //   each node has one receiver
 
                 // configure the transmission range
+                T.add(0);
                 T.add(1);
                 T.add(2);
                 T.add(3);
-                T.add(4);
 
                 // configure the receivers
-                if (id==1 || id==2){
-                    R.add(1);
-                } else if (id==3 || id==4){
+                if (id==0 || id==1){
+                    R.add(0);
+                } else if (id==2 || id==3){
                     R.add(2);
-                } else if (id==5 || id==6){
+                } else if (id==4 || id==5){
                     R.add(3);
-                } else if (id==7 || id==8){
-                    R.add(4);
+                } else if (id==6 || id==7){
+                    R.add(3);
                 }
                 break;
         }
+    }
+
+    private void slotAction(){
+
+        ////////////////////
+        // PACKET ARRIVAL //
+        ////////////////////
+
+        //////////////////////////
+        // Creating trans table //
+        //////////////////////////
+
+        // Initialize the trans table
+        int[] trans = new int[Main.getNumberOfNodes()];
+        for (int i = 0; i < trans.length; i++) {
+            trans[i] = -1;
+        }
+
+        // initialize channels ( Ω )
+        ArrayList<Integer> channels = new ArrayList<>();
+        for (int channel = 0; channel < Main.getNumberOfChannels(); channel++) {
+            channels.add(channel);
+        }
+
+        // get a copy of A
+        ArrayList<ArrayList<Integer>> _A = new ArrayList<>();
+        for (int i = 0; i < A.size(); i++) {
+            _A.add((ArrayList<Integer>) A.get(i).clone());
+        }
+
+        // create trans table
+        while (!channels.isEmpty()) {
+            int k = channels.get(rand.nextInt(channels.size()));        // get a random channel, say channel k
+            int i = _A.get(k).get(rand.nextInt(_A.get(k).size()));      // get a random node from _A[k], say node i
+            trans[i] = k;
+
+            // remove i from _A
+            for (int j = 0; j < _A.size(); j++) {
+                _A.get(j).remove((Integer) i);
+            }
+
+            // remove k from channels (Ω)
+            channels.remove((Integer) k);
+        }
+
+        //////////////////
+        // TRANSMISSION //
+        //////////////////
     }
 }
