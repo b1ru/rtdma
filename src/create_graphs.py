b3764eee3c9eef26ee9a6c25ab6e73dcb84ac215
Bill Tsek <billtsekev@gmail.com>
2021-02-05 17:41:42 +0200

    Change create_graphs.py

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -1,4 +1,6 @@
 import pandas
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 ##############
 # Validation #
@@ -15,3 +17,17 @@ D = list(data['D'])
 tp = [TP[0:8], TP[8:16], TP[16:24], TP[24:32], TP[32:40]]
 q = [Q[0:8], Q[8:16], Q[16:24], Q[24:32], Q[32:40]]
 d = [D[0:8], D[8:16], D[16:24], D[24:32], D[32:40]]
+
+fig = make_subplots(
+rows=3, cols=1,
+specs=[[{"type": "table"}],
+       [{"type": "table"}],
+       [{"type": "table"}]])
+
+fig.add_trace(go.Table(
+    name="b=0.2", header=dict(values=['Node', 'TP', 'Q', 'D']),
+    cells=dict(values=[list(range(1,9)), tp[0], q[0], d[0]])),
+    row=1, col=1
+)
+
+fig.show()
