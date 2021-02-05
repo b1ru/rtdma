542547eaab58853697f9a94af759c9ca0dce1343
Bill Tsek <billtsekev@gmail.com>
2021-02-05 15:10:39 +0200

    Change create_graphs.py

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -8,6 +8,16 @@ import pandas
 #
 
 data = pandas.read_csv('../validation1.csv')
-TP = data['TP']
-Q = data['Q']
-D = data['D']
+TP = list(data['TP'])
+Q = list(data['Q'])
+D = list(data['D'])
+
+throughput = list()
+throughput.append(TP[0:8])
+throughput.append(TP[8:16])
+throughput.append(TP[16:24])
+throughput.append(TP[24:32])
+throughput.append(TP[32:40])
+
+for item in throughput:
+    print(item)
